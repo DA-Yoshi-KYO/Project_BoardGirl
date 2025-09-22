947f944cb2f94f9c3c90b5b918a283c071b98812
AT12A_YOSHIDA <shiguma665hal@gmail.com>
2025-09-22 23:22:36 +0900

    refactor: AddComment Component

--- DX22_Project/Component.cpp
+++ DX22_Project/Component.cpp
@@ -1,12 +1,16 @@
+// インクルード部
 #include "Component.h"
 
 CComponent::CComponent(CGameObject* inGameObject)
     : m_pGameObject(nullptr)
 {
+    // コンポーネントにゲームオブジェクトを紐付ける
     m_pGameObject = inGameObject;
 }
+
 CComponent::~CComponent()
 {
+
 }
 
 void CComponent::Init()
